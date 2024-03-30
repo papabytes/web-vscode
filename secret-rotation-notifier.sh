e234b17204bb71a23ce4ffc8dff6f81d21f20e2b
Joao Abrantes <j.alex.abrantes@gmail.com>
2024-03-30 15:49:18 +0000

    Using mailx instead of ssmtp

--- secret-rotation-notifier.sh
+++ secret-rotation-notifier.sh
@@ -1,39 +1,16 @@
 #!/bin/bash
-#
-MINUTES=$1
-
-if [[ -z $MINUTES ]]
-then
-    MINUTES=5
-fi
-
 getToken() {
-    minutes=$1
-    token="$(docker logs vscode_web --since ${minutes}m 2>&1 | grep -Eo "tkn=.*" | sed s/tkn=//g)"
-    
-    if [ $token != "" ]
-    then
-        echo $token
-    else
-        echo "token is empty. exitting"
-        exit 1
-    fi
+    token="$(docker logs vscode_web 2>&1 | grep -Eo "tkn=.*" | sed s/tkn=//g)"
+    echo "$token"
 }
 
 # Send email
 send_email() {
-    echo "sending email"
     token=$1
-    SUBJECT="VSCode web token"
-
-    echo "Subject: VSCode Web token
-
-    New token is: $token
-    " > mail.txt
-    mailSend=$(/usr/sbin/ssmtp -v REPLACE_ME < mail.txt)
-    echo "email sent"
-    rm mail.txt
+    echo "New Token is $token" | /usr/bin/mailx -s "VS Code Web Token" j.alex.abrantes
 }
 
-token=$(getToken $MINUTES)
-send_email $token
+token=$(getToken)
+if [ "$token" != "" ]; then
+  send_email "$token"
+fi
